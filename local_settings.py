bacf7530340bf4c86bc7caa4104e41b4d56c334b
Qiangning Hong <hongqn@ein.plus>
2016-12-26 16:19:43 +0800

    set DOMAIN to s.einplus.cn

--- local_settings.py
+++ local_settings.py
@@ -2,5 +2,6 @@ import yaml
 
 secrets = yaml.load(open('secrets.yaml'))
 
+DOMAIN = 's.einplus.cn'
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{passwd}@{host}:{port}/{db}'.format(**secrets['mysql'])
